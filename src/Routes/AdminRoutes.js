5e1d5c3950c3db8b6d34ad867158cd05b913fdaf
fruitable <parthvadadoriya317@gmail.com>
2024-03-19 16:43:25 +0530

    LAyout

--- src/Routes/AdminRoutes.js
+++ src/Routes/AdminRoutes.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Product from '../admin/container/Product/Product';
+import Layout from '../admin/component/Layout/Layout';
+import Reviews from '../admin/container/Reviews/Reviews';
 
 function AdminRoutes(props) {
     return (
 
         <div>
+            <Layout>
              <Routes>
                 <Route exact path="/Product" element={<Product />}/>
+                <Route exact path="/Reviews" element={<Reviews />}/>
+
              </Routes>
+             </Layout>
         </div>
     );
 }
